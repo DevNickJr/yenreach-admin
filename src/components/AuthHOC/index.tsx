3b75569ac0b9ca2b813cf7c95f4d39059ddef2f7
DevNickjr <nicholasduadei14@gmail.com>
2024-10-25 11:23:40 +0100

    .

--- src/components/AuthHOC/index.tsx
+++ src/components/AuthHOC/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "src/hooks/useAuthContext";
 
 // import Loader from "../Loader";
@@ -8,16 +9,18 @@ export default function AuthHOC(
 ) {
   //
   return function AuthComp(props: any) {
+    const navigate = useNavigate()
 
     const { isLoggedIn, user } = useAuthContext()
 
 
     if (!isLoggedIn) {
-      return (
-        <div className='flex items-center justify-center w-screen h-screen'>
-         Loading
-        </div>
-      );
+      return navigate('/')
+      // return (
+      //   <div className='flex items-center justify-center w-screen h-screen'>
+      //    Loading
+      //   </div>
+      // );
     }
 
     return <ProtectedComponent user={user} {...props} />;
